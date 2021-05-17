8442b9a486e28f9751f3d0411edc66ea15ef5667
Anoushk Kharangate <anoushka1828@gmail.com>
2021-05-17 14:06:19 +0530

    date issue

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,9 @@ const App = () => {
   // let countries = [];
   const [countries, setCountries] = useState([]);
   const [infected, setInfected] = useState(0);
-  const [recovered, setRecovered]= useState("0")
-  const [deaths, setDeaths]= useState("0")
+  const [recovered, setRecovered] = useState("0");
+  const [deaths, setDeaths] = useState("0");
+  const [date, setDate] = useState("Choose country");
   const fetchCovid = useCallback(async () => {
     const data = await fetch(
       "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
@@ -27,6 +28,7 @@ const App = () => {
     const jsonData = await data.json();
     // jsArray=  [...jsonData];
     console.log(jsonData[1]["country"]);
+
     jsonData.forEach((element) => {
       setCountries((prevCountries) => {
         return [...prevCountries, element["country"]];
@@ -37,20 +39,19 @@ const App = () => {
   useEffect(() => {
     fetchCovid();
   }, [fetchCovid]);
-
   const [whichCountry, selectCountry] = useState("");
+
   const fetchInfected = useCallback(async () => {
     const data = await fetch(
       "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
     );
-    const jsonDataI=await data.json()
-    console.log(whichCountry)
-    jsonDataI.forEach((obj)=>{
-      if(obj['country']==whichCountry){
-        setInfected(obj['infected'])
+    const jsonDataI = await data.json();
+    console.log(whichCountry);
+    jsonDataI.forEach((obj) => {
+      if (obj["country"] == whichCountry) {
+        setInfected(obj["infected"]);
       }
-    })
-
+    });
   });
   useEffect(() => {
     fetchInfected();
@@ -60,14 +61,13 @@ const App = () => {
     const data = await fetch(
       "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
     );
-    const jsonDataR=await data.json()
-    console.log(whichCountry)
-    jsonDataR.forEach((obj)=>{
-      if(obj['country']==whichCountry){
-        setRecovered(obj['recovered'])
+    const jsonDataR = await data.json();
+    console.log(whichCountry);
+    jsonDataR.forEach((obj) => {
+      if (obj["country"] == whichCountry) {
+        setRecovered(obj["recovered"]);
       }
-    })
-
+    });
   });
   useEffect(() => {
     fetchRecovered();
@@ -77,17 +77,39 @@ const App = () => {
     const data = await fetch(
       "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
     );
-    const jsonDataD=await data.json()
-    console.log(whichCountry)
-    jsonDataD.forEach((obj)=>{
-      if(obj['country']==whichCountry){
-        setDeaths(obj['deceased'])
+    const jsonDataD = await data.json();
+    console.log(whichCountry);
+    jsonDataD.forEach((obj) => {
+      if (obj["country"] == whichCountry) {
+        setDeaths(obj["deceased"]);
       }
-    })
-
+    });
   });
   useEffect(() => {
     fetchDeaths();
+  }, [whichCountry]);
+
+  const fetchDate = useCallback(async () => {
+    const data = await fetch(
+      "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
+    );
+    const jsonDatad = await data.json();
+    // jsArray=  [...jsonData];
+    console.log(jsonDatad[1]["country"]);
+
+    jsonDatad.forEach((element) => {
+      setDate(() => {
+        console.log(element["country"], whichCountry);
+        if (element["country"] == whichCountry) {
+          console.log(typeof element["lastUpdatedApify"]);
+          return (element["lastUpdatedApify"]);
+        }
+      });
+      console.log(date+"in")
+    });
+  });
+  useEffect(() => {
+    fetchDate();
   }, [whichCountry]);
 
   // const addCountry=(param)=>{
@@ -98,10 +120,13 @@ const App = () => {
     selectCountry(e.target.value);
     console.log(whichCountry);
   };
-
+  console.log(date);
   return (
     <VStack
-      bgGradient={useColorModeValue('linear(to-br,#56CCF2, #2F80ED)', 'linear(to-br,#000046, #1CB5E0)')}
+      bgGradient={useColorModeValue(
+        "linear(to-br,#56CCF2, #2F80ED)",
+        "linear(to-br,#000046, #1CB5E0)"
+      )}
       spacing={4}
       align="stretch"
     >
@@ -113,7 +138,7 @@ const App = () => {
         <HStack mt={20} spacing="24px">
           <Stat_card
             count={infected}
-            date="11/05/21"
+            date={date?date:"data unavailable"}
             desc="Total infections"
             txtcol="red.400"
             img="https://image.flaticon.com/icons/png/128/2659/2659980.png"
@@ -121,7 +146,7 @@ const App = () => {
           ></Stat_card>
           <Stat_card
             count={recovered}
-            date="11/05/21"
+            date={date?date:"data unavailable"}
             txtcol="blue.300"
             tag="Recovered"
             desc="Total number of people recovered"
@@ -129,7 +154,7 @@ const App = () => {
           ></Stat_card>
           <Stat_card
             count={deaths}
-            date="11/05/21"
+            date={date?date:"data unavailable"}
             txtcol="gray.400"
             tag="Deaths"
             desc="Total Number of Deaths"
