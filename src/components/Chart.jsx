baa9577b2d21d707e153366ee77ea97e104e9c20
Anoushk Kharangate <anoushka1828@gmail.com>
2021-05-20 15:52:56 +0530

    chart issue

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -9,7 +9,7 @@ import {
   CartesianGrid,
 } from "recharts";
 import { Box, useColorModeValue, Select, Flex } from "@chakra-ui/react";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 // defaults.global.tooltips.enabled = false
 // defaults.global.legend.position = 'bottom'
 import { format, parseISO, set, subDays } from "date-fns";
@@ -21,9 +21,12 @@ for (let num = 30; num >= 0; num--) {
     value: 1 + Math.random(),
   });
 }
-const BarChart = (whichCountry) => {
+//const ydata=[{ylabel:6000},{ylabel:12000},{ylabel:18000},{ylabel:24000}]
+//var chartTemp=[];
+const BarChart = ({ whichCountry }) => {
   const [chartdata, setChartdata] = useState([]);
   const [opt, setopt] = useState("Choose an option");
+  const [chartTemp, setChartTemp] = useState("0");
   const fetchChartData = async () => {
     const data = await fetch(
       "https://api.apify.com/v2/key-value-stores/tVaYRsPHLjNdNBu7S/records/LATEST?disableRedirect=true"
@@ -33,42 +36,73 @@ const BarChart = (whichCountry) => {
     //console.log(jsonData[1]["country"]);
 
     jsonData.forEach(async (element) => {
-      console.log(element["country"],whichCountry.whichCountry)
-      if (element["country"] == whichCountry.whichCountry) {
+      //console.log(element["country"],whichCountry)
+      if (element["country"] == whichCountry) {
         //console.log(element["historyData"])
+        setChartdata("0");
+        setChartTemp("0");
         const data2 = await fetch(element["historyData"]);
         const jsonData2 = await data2.json();
-        // jsonData2.forEach((element)=>{
-        //    if(element.hasOwnProperty('infected')==true){
-        //        return masterData.push([...element['infected']])
-        //    }else {
-        //        return [...element['activeCases']]
-        //    }
-        // })
+
         for (const key of Object.keys(jsonData2)) {
-          if (opt == "infected") {
-            var cases = jsonData2[key];
-            if (cases.hasOwnProperty("infected")) {
-              setChartdata((previtem) => {
-                return [...previtem, cases["infected"]];
-              });
+          //console.log(key, opt);
+          var cases = await jsonData2[key];
+          if (opt == "Infected") {
+            if (
+              cases.hasOwnProperty("infected") &&
+              cases["infected"] != undefined
+            ) {
+              setChartTemp(JSON.stringify(cases["infected"]));
+              //console.log(chartdata)
+            } else if (
+              cases.hasOwnProperty("totalCases") &&
+              cases["totalCases"] != undefined
+            ) {
+              setChartTemp(JSON.stringify(cases["totalCases"]));
             } else {
-              setChartdata((prevItem) => {
-                return [...prevItem, cases["totalCases"]];
-              });
+              continue;
+            }
+          } else if (opt == "Recovered") {
+            if (
+              cases.hasOwnProperty("recovered") &&
+              cases["recovered"] != undefined
+            ) {
+              setChartTemp(JSON.stringify(cases["recovered"]));
+            } else {
+              continue;
+            }
+          } else if (opt == "Deceased") {
+            if (
+              cases.hasOwnProperty("deceased") &&
+              cases["deceased"] != undefined
+            ) {
+              setChartTemp(JSON.stringify(cases["deceased"]));
+            } else if (
+              cases.hasOwnProperty("deaths") &&
+              cases["deaths"] != undefined
+            ) {
+              setChartTemp(JSON.stringify(cases["deaths"]));
+            } else {
+              continue;
             }
           }
         }
       }
     });
-    
   };
 
   useEffect(() => {
     fetchChartData();
-    console.log(chartdata)
-  }, [opt]);
-  
+    //console.log(chartTemp)
+  }, [opt, whichCountry]);
+
+  useEffect(() => {
+    setChartdata(() => {
+      return [...chartdata, { ykey: chartTemp }];
+    });
+    //console.log(chartdata);
+  }, [chartTemp]);
+
   const addOpt = (e) => {
     setopt(e.target.value);
   };
@@ -84,44 +118,20 @@ const BarChart = (whichCountry) => {
     >
       <Flex justify="center">
         <Select mb="1.5" width="47%" variant="filled" onChange={addOpt}>
-          <option defaultValue>{opt}</option>
+          <option defaultValue>Choose an option</option>
           <option>Infected</option>
           <option>Recovered</option>
           <option>Deceased</option>
         </Select>
       </Flex>
       <div>
-        <ResponsiveContainer width="100%" height={500}>
-          <AreaChart data={data}>
-            <defs>
-              <linearGradient id="color" x1="0" y1="0" x2="0" y2="1">
-                <stop offset="0%" stopColor="#2451B7" stopOpacity={0.4} />
-                <stop offset="75%" stopColor="#2451B7" stopOpacity={0.05} />
-              </linearGradient>
-            </defs>
-
-            <Area dataKey="value" stroke="#2451B7" fill="url(#color)" />
-
-            <XAxis
-              dataKey="date"
-              axisLine={false}
-              tickLine={false}
-              tickFormatter={(str) => {
-                return opt;
-              }}
-            />
-
-            <YAxis
-              datakey="value"
-              axisLine={false}
-              tickLine={false}
-              tickCount={8}
-              tickFormatter={(number) => `$${number.toFixed(2)}`}
-            />
-
-            <CartesianGrid opacity={0.1} vertical={false} />
-          </AreaChart>
-        </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height={500}>
+      <AreaChart data={chartdata.length > 1 ? chartdata : data}>
+        <Area dataKey="ykey"/>
+        <XAxis dataKey="data"/>
+        <YAxis dataKey="ykey"/>
+      </AreaChart>
+    </ResponsiveContainer>
       </div>
     </Box>
   );
